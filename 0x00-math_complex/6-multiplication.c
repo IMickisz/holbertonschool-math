4b618460e44fdc8d4b77d853f5f6b101831eab3d
Ivan Mickisz <ivan.mickisz3@gmail.com>
2021-11-12 23:27:44 +0100

    corrected wrong function name

--- 0x00-math_complex/6-multiplication.c
+++ 0x00-math_complex/6-multiplication.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
 
 /**
-* substraction - return the substraction
+* multiplication - return the multiplication
 * @c1: complex
 * @c2: complex
 * @c3: complex pointer
 * Return: void
 */
 
-void substraction(complex c1, complex c2, complex *c3)
+void multiplication(complex c1, complex c2, complex *c3)
 {
-	c3->re = c1.re - c2.re;
-	c3->im = c1.im - c2.im;
+	c3->re = (c1.re * c2.re) - (c1.im * c2.im);
+	c3->im = (c1.re * c2.im) + (c1.im * c2.re);
 }
